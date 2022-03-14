fee4218b3c100df7232926f2c82ea905bc684a19
Miguel Rodrigues <migb.rodrigues@gmail.com>
2022-03-14 11:40:13 +0000

    changing the implementation from a nested vector implementation to a single block implementation

--- matrixproduct/src/main.rs
+++ matrixproduct/src/main.rs
@@ -1,12 +1,14 @@
 use std::time::Instant;
 
 fn mul_by_cols(m_len: usize) {
-    let m_a: Vec<Vec<f32>> = vec![vec![1.0; m_len]; m_len];
-    let mut m_b: Vec<Vec<f32>> = vec![];
-    let mut m_c: Vec<Vec<f32>> = vec![vec![0.0; m_len]; m_len];
+    let m_a: Vec<f32> = vec![1.0; m_len * m_len];
+    let mut m_b: Vec<f32> = vec![0.0; m_len * m_len];
+    let mut m_c: Vec<f32> = vec![0.0; m_len * m_len];
 
     for i in 0..m_len {
-        m_b.insert(i, vec![(i+1) as f32; m_len]);
+        for j in 0..m_len {
+            m_b[i * m_len + j] = (i+1) as f32;
+        }
     }
 
     let start = Instant::now();
@@ -14,26 +16,28 @@ fn mul_by_cols(m_len: usize) {
     for i in 0..m_len {
         for j in 0..m_len {
             for k in 0..m_len {
-                m_c[i][j] += m_a[i][k] * m_b[k][j]
+                m_c[i * m_len + j] += m_a[i * m_len + k] * m_b[k * m_len + j]
             }
         }
     }
 
     let elapsed = start.elapsed();
-    println!("elapsed time [mul_by_cols]: {:.3?}", elapsed);
+    println!("elapsed time [mul_by_cols]: {elapsed:.3?}");
 
     let elem : usize = m_len * (m_len+1)/2;
-    assert_eq!(m_c[0][0], elem as f32);
+    assert_eq!(m_c[0], elem as f32);
     println!("passed arithmetic progression test: {elem} (m_len: {m_len})");
 }
 
 fn mul_by_line(m_len: usize) {
-    let m_a: Vec<Vec<f32>> = vec![vec![1.0; m_len]; m_len];
-    let mut m_b: Vec<Vec<f32>> = vec![];
-    let mut m_c: Vec<Vec<f32>> = vec![vec![0.0; m_len]; m_len];
+    let m_a: Vec<f32> = vec![1.0; m_len * m_len];
+    let mut m_b: Vec<f32> = vec![0.0; m_len * m_len];
+    let mut m_c: Vec<f32> = vec![0.0; m_len * m_len];
 
     for i in 0..m_len {
-        m_b.insert(i, vec![(i+1) as f32; m_len]);
+        for j in 0..m_len {
+            m_b[i * m_len + j] = (i+1) as f32;
+        }
     }
 
     let start = Instant::now();
@@ -41,16 +45,16 @@ fn mul_by_line(m_len: usize) {
     for i in 0..m_len {
         for k in 0..m_len {
             for j in 0..m_len {
-                m_c[i][j] += m_a[i][k] * m_b[k][j]
+                m_c[i * m_len + j] += m_a[i * m_len + k] * m_b[k * m_len + j]
             }
         }
     }
 
     let elapsed = start.elapsed();
-    println!("elapsed time [mul_by_line]: {:.3?}", elapsed);
+    println!("elapsed time [mul_by_line]: {elapsed:.3?}");
 
     let elem : usize = m_len * (m_len+1)/2;
-    assert_eq!(m_c[0][0], elem as f32);
+    assert_eq!(m_c[0], elem as f32);
     println!("passed arithmetic progression test: {elem} (m_len: {m_len})");
 }
 
